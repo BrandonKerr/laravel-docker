78bf3637147a6fc6b90bf900b686d8f3ea90f086
Brandon Kerr <brandon.js.kerr@gmail.com>
2026-03-04 21:18:04 -0500

    refactor: drop unnecessary SQL init

--- docker/mariadb/init.sql
+++
@@ -1,7 +0,0 @@
--- Create database if it doesn't exist
-CREATE DATABASE IF NOT EXISTS `my_project`;
--- Create application user
-CREATE USER IF NOT EXISTS 'laravel'@'%' IDENTIFIED BY 'secret';
--- Grant privileges
-GRANT ALL PRIVILEGES ON `my_project`.* TO 'laravel'@'%';
-FLUSH PRIVILEGES;
